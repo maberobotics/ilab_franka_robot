5d8882e9b896f4a3df14c7eb67d8b17b3b452a97
mcbed <mcbed.robotics@gmail.com>
2023-12-01 17:08:34 +0100

    fixed typos

--- ilab_franka_bringup/launch/ilab_franka.launch.py
+++ ilab_franka_bringup/launch/ilab_franka.launch.py
@@ -260,8 +260,8 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['franka_arm_controller', '-c', [namespace, 'controller_manager']]
-        ondition=IfCondition(use_fake_hardware) or IfCondition(use_sim)
+        arguments=['franka_arm_controller', '-c', [namespace, 'controller_manager']],
+        condition=IfCondition(use_fake_hardware) or IfCondition(use_sim)
     )
 
     robot_effort_controller_spawner = Node(
